ab859a7438e593f3fdc02946e281f7860a1d80d0
Jumas-Cola <kbbyfl91@gmail.com>
2019-03-17 21:09:55 +0300

    upd

--- pascalABC.NET_fractals/fractals.pas
+++ pascalABC.NET_fractals/fractals.pas
@@ -1,240 +1,196 @@
-﻿uses GraphABC;
+﻿unit fractals;
 
-var a:real; // глобальная переменная текущего угла
+interface
+
+uses GraphABC;
+
+var
+    a: real;// глобальная переменная текущего угла
+
+procedure go_forward(len: real; angle: real);// отрисовка прямой заданной длины и заданного наклона
+procedure koch(order: integer; size: real);// отрисовка кривой Коха
+procedure mink(order: integer; size: real);// отрисовка кривой Минковского
+procedure levi(order: integer; size: real);// отрисовка кривой Леви
+procedure dragon(order: integer; size: real; dir: char := 'r');// отрисовка кривой дракона
+procedure serp(order: integer; size: real; dir: char := 'f');//приближённая отрисовка треугольника Серпинского
+procedure chaos_serp(arr: array of integer);//процедура отрисовки ковра Серпинского методом хаоса
+procedure chaos_barns(size, padding_left, padding_bottom: integer);//процедура отрисовки папортника Барнсли
+
+implementation
 
 // отрисовка прямой заданной длины и заданного наклона
-procedure go_forward(len:real; angle:real);
-  var cur_pos:array[1..2] of integer;
-  begin
-    cur_pos[1]:=penx();
-    cur_pos[2]:=peny();
-    lineto(cur_pos[1]+round(len*cos(angle)), cur_pos[2]+round(len*sin(angle)));
-  end;
+procedure go_forward;
+var
+    cur_pos: array[1..2] of integer;
+begin
+    cur_pos[1] := penx();
+    cur_pos[2] := peny();
+    lineto(cur_pos[1] + round(len * cos(angle)), cur_pos[2] + round(len * sin(angle)));
+end;
 
 // отрисовка кривой Коха
-procedure koch(order:integer; size:real);
-  begin
-  if order=0 then
-    go_forward(size, a)
-  else
+procedure koch;
+begin
+    if order = 0 then
+        go_forward(size, a)
+    else
     begin
-      koch(order-1, size/3);
-      a-=Pi/3;
-      koch(order-1, size/3);
-      a+=2*Pi/3;
-      koch(order-1, size/3);
-      a-=Pi/3;
-      koch(order-1, size/3);
+        koch(order - 1, size / 3);
+        a -= Pi / 3;
+        koch(order - 1, size / 3);
+        a += 2 * Pi / 3;
+        koch(order - 1, size / 3);
+        a -= Pi / 3;
+        koch(order - 1, size / 3);
     end;
-  end;
+end;
 
 // отрисовка кривой Минковского
-procedure mink(order:integer; size:real);
-  begin
-  if order=0 then
-    go_forward(size, a)
-  else
+procedure mink;
+begin
+    if order = 0 then
+        go_forward(size, a)
+    else
     begin
-      mink(order-1, size/8);
-      a-=Pi/2;
-      mink(order-1, size/8);
-      a+=Pi/2;
-      mink(order-1, size/8);
-      a+=Pi/2;
-      mink(order-1, size/8);
-      mink(order-1, size/8);
-      a-=Pi/2;
-      mink(order-1, size/8);
-      a-=Pi/2;
-      mink(order-1, size/8);
-      a+=Pi/2;
-      mink(order-1, size/8);
+        mink(order - 1, size / 8);
+        a -= Pi / 2;
+        mink(order - 1, size / 8);
+        a += Pi / 2;
+        mink(order - 1, size / 8);
+        a += Pi / 2;
+        mink(order - 1, size / 8);
+        mink(order - 1, size / 8);
+        a -= Pi / 2;
+        mink(order - 1, size / 8);
+        a -= Pi / 2;
+        mink(order - 1, size / 8);
+        a += Pi / 2;
+        mink(order - 1, size / 8);
     end;
-  end;
+end;
 
 // отрисовка кривой Леви
-procedure levi(order:integer; size:real);
-  begin
-  if order=0 then
-    go_forward(size, a)
-  else
+procedure levi;
+begin
+    if order = 0 then
+        go_forward(size, a)
+    else
     begin
-      a-=Pi/4;
-      levi(order-1, size);
-      a+=Pi/2;
-      levi(order-1, size);
-      a-=Pi/4;
+        a -= Pi / 4;
+        levi(order - 1, size);
+        a += Pi / 2;
+        levi(order - 1, size);
+        a -= Pi / 4;
     end;
-  end;
-  
-// отрисовка кривой дракона
-procedure dragon(order:integer; size:real; dir:char:='r');
-  begin
-  if order=0 then
-    go_forward(size, a)
-  else
-    begin
-      if dir='r' then
-        begin
-          a+=Pi/4;
-          dragon(order-1, size/sqrt(2), 'r');
-          a-=Pi/2;
-          dragon(order-1, size/sqrt(2), 'l');
-          a+=Pi/4;
-        end
-      else
-        begin
-          a-=Pi/4;
-          dragon(order-1, size/sqrt(2), 'r');
-          a+=Pi/2;
-          dragon(order-1, size/sqrt(2), 'l');
-          a-=Pi/4;
-        end;
-    end;
-  end;
+end;
 
-// отрисовка треугольника Серпинского
-procedure serp(order:integer; size:real; dir:char:='f');
-  begin
-  if order=0 then
-    go_forward(size, a)
-  else
+// отрисовка кривой дракона
+procedure dragon;
+begin
+    if order = 0 then
+        go_forward(size, a)
+    else
     begin
-      if dir='f' then
+        if dir = 'r' then
         begin
-          if order mod 2<>0 then
-            a-=Pi/3;
-          dir:='r';
-        end;
-      if dir='r' then
-        begin
-          serp(order-1, size/3, 'l');
-          a+=Pi/3;
-          serp(order-1, size/3, 'r');
-          a+=Pi/3;
-          serp(order-1, size/3, 'l');
+            a += Pi / 4;
+            dragon(order - 1, size / sqrt(2), 'r');
+            a -= Pi / 2;
+            dragon(order - 1, size / sqrt(2), 'l');
+            a += Pi / 4;
         end
-      else if dir='l' then
+        else
         begin
-          serp(order-1, size/3, 'r');
-          a-=Pi/3;
-          serp(order-1, size/3, 'l');
-          a-=Pi/3;
-          serp(order-1, size/3, 'r');
+            a -= Pi / 4;
+            dragon(order - 1, size / sqrt(2), 'r');
+            a += Pi / 2;
+            dragon(order - 1, size / sqrt(2), 'l');
+            a -= Pi / 4;
         end;
     end;
-  end;
+end;
+
+//приближённая отрисовка треугольника Серпинского
+procedure serp;
+begin
+    if order = 0 then
+        go_forward(size, a)
+    else
+    begin
+        if dir = 'f' then
+        begin
+            if order mod 2 <> 0 then
+                a -= Pi / 3;
+            dir := 'r';
+        end;
+        if dir = 'r' then
+        begin
+            serp(order - 1, size / 3, 'l');
+            a += Pi / 3;
+            serp(order - 1, size / 3, 'r');
+            a += Pi / 3;
+            serp(order - 1, size / 3, 'l');
+        end
+        else if dir = 'l' then
+        begin
+            serp(order - 1, size / 3, 'r');
+            a -= Pi / 3;
+            serp(order - 1, size / 3, 'l');
+            a -= Pi / 3;
+            serp(order - 1, size / 3, 'r');
+        end;
+    end;
+end;
 
 //процедура отрисовки ковра Серпинского методом хаоса
-procedure chaos_serp(a:array of integer);
-var x:integer;
+procedure chaos_serp;
+var
+    x: integer;
 begin
-var center := window.center;
-moveto(center.x, center.y);
-randomize;
-for i:integer:=0 to 100000 do
-  begin
-    x:=(random(round(length(a)/2))+1)*2-1;
-    PutPixel(round((a[x-1]+penx())/2), round((a[x]+peny())/2), RGB(0,0,0));
-    MoveTo(round((a[x-1]+penx())/2), round((a[x]+peny())/2));
-  end;
+    var center := window.center;
+    moveto(center.x, center.y);
+    randomize;
+    for i: integer := 0 to 100000 do
+    begin
+        x := (random(round(length(arr) / 2)) + 1) * 2 - 1;
+        PutPixel(round((arr[x - 1] + penx()) / 2), round((arr[x] + peny()) / 2), RGB(0, 0, 0));
+        MoveTo(round((arr[x - 1] + penx()) / 2), round((arr[x] + peny()) / 2));
+    end;
 end;
 
 //процедура отрисовки папортника Барнсли
-procedure chaos_barns(size,padding_left,padding_bottom:integer);
+procedure chaos_barns;
 begin
-randomize;
-var i:integer;
-var x:real;
-var y:real;
-for j:integer:=0 to 100000 do
-  begin
-    i:=random(100);
-    if i in [0] then
-      begin
-        x := 0;
-        y := 0.16*peny();
-      end
-    else if i in [1..85] then
-      begin
-        x := 0.85*penx() + 0.04*peny();
-        y := -0.04*penx() + 0.85*peny() + 1.6;
-      end
-    else if i in [86..92] then
-      begin
-        x := 0.2*penx() - 0.26*peny();
-        y := 0.23*penx() + 0.22*peny() + 1.6;
-      end
-    else if i in [93..99] then
-      begin
-        x := -0.15*penx() + 0.28*peny();
-        y := 0.26*penx() + 0.24*peny() + 0.44;
-      end;
-    PutPixel(round(x+padding_left), window.height-round(y+padding_bottom), RGB(0,100,0));
-    MoveTo(round(x), round(y+size));
-  end;
+    randomize;
+    var i: integer;
+    var x: real;
+    var y: real;
+    for j: integer := 0 to 100000 do
+    begin
+        i := random(100);
+        if i in [0] then
+        begin
+            x := 0;
+            y := 0.16 * peny();
+        end
+        else if i in [1..85] then
+        begin
+            x := 0.85 * penx() + 0.04 * peny();
+            y := -0.04 * penx() + 0.85 * peny() + 1.6;
+        end
+        else if i in [86..92] then
+        begin
+            x := 0.2 * penx() - 0.26 * peny();
+            y := 0.23 * penx() + 0.22 * peny() + 1.6;
+        end
+        else if i in [93..99] then
+        begin
+            x := -0.15 * penx() + 0.28 * peny();
+            y := 0.26 * penx() + 0.24 * peny() + 0.44;
+        end;
+        PutPixel(round(x + padding_left), window.height - round(y + padding_bottom), RGB(0, 100, 0));
+        MoveTo(round(x), round(y + size));
+    end;
 end;
 
-begin
-  var center:=window.center;
-  var size:=300; // длина сегмента
-  var order:=5; // порядок рекурсии
-  moveto(center.x-200, center.y-100); // установка указателя на 100 пикселей выше и на 200 левее центра
-  
-  a:=0; // начальное значение угла
-  // отрисовка трёх кривых Коха под углом в 120 градусов (снежинка Коха)
-  koch(order,size);
-  a+=2*Pi/3;
-  koch(order,size);
-  a+=2*Pi/3;
-  koch(order,size);
-  
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-  
-  a:=0; // начальное значение угла
-  // отрисовка кривой Минковского
-  mink(3,size*10);
-  a+=Pi/2;
-  mink(3,size*10);
-  a+=Pi/2;
-  mink(3,size*10);
-  a+=Pi/2;
-  mink(3,size*10);
-    
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-    moveto(center.x+100, center.y); // установка указателя
-  
-  a:=Pi; // начальное значение угла
-  // отрисовка кривой Леви
-  levi(9,size/30);
-  
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-  
-  a:=0; // начальное значение угла
-  // отрисовка кривой дракона
-  dragon(15,size);
-  
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-    moveto(center.x-250, center.y+200); // установка указателя
-  
-  a:=0; // начальное значение угла
-  // отрисовка треугольника Серпинского
-  serp(8,size*37);
-  
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-  
-  //отрисовка ковра Серпинского методом хаоса
-  var arr: array of integer := (center.x, 10, center.x-200, 400, center.x+200, 400);
-  chaos_serp(arr);
-  
-    Sleep(1000); // пауза
-    ClearWindow(); // очистка окна
-    
-  //процедура отрисовки папортника Барнсли
-  chaos_barns(80,300,20);
 end.
